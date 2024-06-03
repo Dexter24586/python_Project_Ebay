8e38a33691c38ed0a4a2fc207ec988d99972813c
Viktor S <skuridin.ub@gmail.com>
2024-06-03 10:59:04 -0400

    last project 6/3

--- features/environment.py
+++ features/environment.py
@@ -13,7 +13,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 def before_all(context):
-    ...
+    context.username = 'Test User'
+    context.password = os.environ['SECRET']
 def before_feature(context, feature):
     context.url = "https://www.ebay.com"
 def before_scenario(context, scenario):
@@ -27,7 +28,8 @@ def after_step(context, step):
 
         context.driver.save_screenshot(os.path.join(relative_path_to_dest, f'{step.name}.png'))
 def after_scenario(context, scenario):
-    ...
+    context.driver.close()
+    context.driver.quit()
 def after_feature(context, feature):
     ...
 def after_all(context):
